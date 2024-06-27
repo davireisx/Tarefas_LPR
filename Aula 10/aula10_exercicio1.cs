259c8d152c5a5cd7c1eea61fd788e142a739c1ab
davireisx <davireis530@gmail.com>
2024-06-27 15:06:55 -0300

    Update aula10_exercicio1.cs

--- Aula 10/aula10_exercicio1.cs
+++ Aula 10/aula10_exercicio1.cs
@@ -4,56 +4,36 @@ using System.Collections.Generic;
 class HelloWorld {
   static void Main() {
     
-    List <string> nomes = new List<string>();
-    List <string> nomesOrdem = new List<string>();
+    List <string> nomes = new List <string> ();
+     List<List<string>> ordenado = new List<List<string>>(15);
     
-    Console.WriteLine("Quanto nomes serão inseridos?");
-    uint quant = uint.Parse(Console.ReadLine());
-    Console.Write("Insira um nome: ");
+    Console.Write("Digite a quantidade de nomes que serão inseridos na lista: ");
+    int quantidade = int.Parse(Console.ReadLine());
     
-    for(int i=0; i<quant; i++) {
+    for(int i=0; i < quantidade; i++) {
         
-        if(i==0){
-        } else if(i!=quant-1){
-            Console.Write("Insira um outro nome: ");
-        } else {
-            Console.Write("Insira o último nome: ");
-        }
-        string anem = Console.ReadLine();
-        nomes.Add(anem);
-        
+    Console.Write($"Insira o nome {i+1}: ");
+    string name = Console.ReadLine();
+    nomes.Add(name);
     }
-    
-    int tamanho, tamanhoant = 0;
-    
-    while(nomes.Count != 0) {
-        
-        foreach(string nome in nomes) {
-        
-        tamanho = nome.Length;
-        if(tamanho < tamanhoant){
-            nomesOrdem.Insert(nome)
+
+        for (int i = 0; i < 15; i++) {
+            ordenado.Add(new List<string>());
         }
-        tamanhoant = tamanho;
-        nomes.Remove(nome);
-        
+
+        foreach (var nome in nomes) {
+            int tamanho = nome.Length;
+            if (tamanho < 15) {
+                ordenado[tamanho].Add(nome);
+            }
+        }
+
+        for (int i = 0; i < ordenado.Count; i++) {
+            if (ordenado[i].Count > 0) {
+                foreach (var nome in ordenado[i]) {
+                    Console.WriteLine(nome);
+                }
+            }
         }
     }
-    
-    
-    while(nomes.Count != 0) {
-        
-        foreach(string nome in nomes) {
-        
-        tamanho = nome.Length;
-        if(tamanho != tamanhoant){
-        Console.Write(nome);
-        }
-        tamanhoant = tamanho;
-        nomes.Remove(nome);
-        
-        }
-        Console.WriteLine("");
-}
-}
 }
