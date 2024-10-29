aa3d8794192c01033ec420de897fd53cac0834be
davireisx <davireis530@gmail.com>
2024-10-28 21:54:22 -0300

    aula 11
    exercicio 1

---
+++ Aula 11/Program.cs
@@ -0,0 +1,22 @@
+﻿using Aula_11;
+using System;
+
+namespace Aula_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.Write("Insira a altura do retângulo: ");
+            retangulo.altura = double.Parse(Console.ReadLine());
+            Console.Write("Insira a largura do retângulo: ");
+            retangulo.largura = double.Parse(Console.ReadLine());
+            retangulo.calcArea();
+            retangulo.calcPerimetro();
+
+            Console.WriteLine($"A área do triângulo é {retangulo.area}");
+            Console.WriteLine($"O perímetro do triângulo é {retangulo.perimetro}");
+        }
+    }
+}
